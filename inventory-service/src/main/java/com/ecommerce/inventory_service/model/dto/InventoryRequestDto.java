8b4d9a06124697085e2e2d91ffdb40a589451130
Roger Rojas <byrogr.dev@gmail.com>
2026-08-23 12:38:23 -0500

    feat(ms-order): add inventory model and DTOs
    - configure MySQL

---
+++ inventory-service/src/main/java/com/ecommerce/inventory_service/model/dto/InventoryRequestDto.java
@@ -0,0 +1,12 @@
+package com.ecommerce.inventory_service.model.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+public class InventoryRequestDto {
+    @NotBlank(message = "SKU must not be blank")
+    private String sku;
+
+    @Min(value = 0, message = "Quantity must be greater than or equal to 0")
+    private Integer quantity;
+}
---
+++ inventory-service/src/main/java/com/ecommerce/inventory_service/model/dto/InventoryResponseDto.java
@@ -0,0 +1,17 @@
+package com.ecommerce.inventory_service.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class InventoryResponseDto {
+    private Long id;
+    private String sku;
+    private Integer quantity;
+    private boolean isInStock;
+}
