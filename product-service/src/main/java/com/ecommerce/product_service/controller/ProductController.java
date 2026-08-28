b0df96cef44923039cdb546c8f45a1574eabd232
Roger Rojas <byrogr.dev@gmail.com>
2026-08-27 23:37:44 -0500

    feat(product-service): add actuator dependency

--- product-service/src/main/java/com/ecommerce/product_service/controller/ProductController.java
+++ product-service/src/main/java/com/ecommerce/product_service/controller/ProductController.java
@@ -3,9 +3,12 @@ package com.ecommerce.product_service.controller;
 import com.ecommerce.product_service.model.dto.ProductRequestDto;
 import com.ecommerce.product_service.model.dto.ProductResponseDto;
 import com.ecommerce.product_service.service.ProductService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,13 +19,18 @@ import java.util.List;
 @RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
 @Slf4j
+@RefreshScope
 public class ProductController {
     private final ProductService productService;
 
+    @Value("${app.maintenance.message: OS}")
+    private String maintenanceMessage;
+
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<ProductResponseDto> getAllProducts() {
+    public List<ProductResponseDto> getAllProducts(HttpServletResponse response) {
         log.info("** Init fetching all products **");
+        response.addHeader("X-Maintenance-Message", maintenanceMessage);
         return productService.getAllProducts();
     }
 
